8adfde02eb30ef413d8769e8999266c9bf396fe0
Diogo Barros <Diogobarros.26.11@gmail.com>
2026-03-04 15:43:15 -0300

    Adicionando projeto completo

---
+++ AgendaCrud_1/AgendaCrud_1/Infrastructure/Data/AppDbContext.cs.cs
@@ -0,0 +1,14 @@
+﻿using AgendaCrud_1.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaCrud_1.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Contact> Contacts { get; set; }
+}
