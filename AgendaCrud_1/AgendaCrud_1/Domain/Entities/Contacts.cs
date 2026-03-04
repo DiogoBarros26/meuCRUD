33e0b8989d3c3f0f4382910567ba436a683ced7f
Diogo Barros <Diogobarros.26.11@gmail.com>
2026-03-04 16:08:05 -0300

    Removendo arquivos desnecessários

--- AgendaCrud_1/AgendaCrud_1/Domain/Entities/Contacts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AgendaCrud_1.Domain.Entities;
-
-public class Contact
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-}
